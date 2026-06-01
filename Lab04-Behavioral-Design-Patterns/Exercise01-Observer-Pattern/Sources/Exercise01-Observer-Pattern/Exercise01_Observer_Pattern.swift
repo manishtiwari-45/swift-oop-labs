1b5877ac9e9325e9aea78f15ae6773e11cc14c74
Manish Tiwari <manish.kumar.tiwari@beans.ai>
2026-06-01 11:40:48 +0530

    added file

--- Lab04-Behavioral-Design-Patterns/Exercise01-Observer-Pattern/Sources/Exercise01-Observer-Pattern/Exercise01_Observer_Pattern.swift
+++
@@ -1,9 +0,0 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-@main
-struct Exercise01_Observer_Pattern {
-    static func main() {
-        print("Hello, world!")
-    }
-}
